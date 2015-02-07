2478707eeee294b9e04f15e23da9e5ef5d9a793d
Ali Eskandari <alieskandari3@gmail.com>
2015-02-07 13:38:38 -0500

    Added gun, and sound
    Added gun that shoots spheres
    Added sound for when bullet is shot

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -13,10 +13,10 @@ public class Follow : MonoBehaviour {
 
 	void FixedUpdate() 
 	{
-		Vector3 vectorToPlayer = new Vector3 (player.transform.position.x - transform.position.x, 0.0f, player.transform.position.z - transform.position.z);
+		Vector3 vectorToPlayer = new Vector3 (player.transform.position.x - transform.position.x, rigidbody.velocity.y, player.transform.position.z - transform.position.z);
 		Vector3 currentVelocity = rigidbody.velocity;
 		Vector3 finalVector = vectorToPlayer - currentVelocity;
-		Debug.Log (player.transform.position);
+		
 		rigidbody.AddForce (finalVector * speed * Time.deltaTime);
 	}
 
