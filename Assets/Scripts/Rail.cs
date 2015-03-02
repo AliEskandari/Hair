6ae1f8bb124c42ac4ae8cca5ff9a38ec8e42bf53
Ali Eskandari <alieskandari3@gmail.com>
2015-03-02 01:13:53 -0500

    Changed hair parameters and made 4 of them
    hair is more like we want

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+//		transform.position.z = 1;
+	}
+}
